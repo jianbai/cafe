81633f445c909be121b75afb3d77f7dc96300abe
spw <spw@spws-MacBook-Pro.local>
2015-09-13 20:02:42 -0700

    add xcode files

---
+++ client/ios/cafe/cafe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cafe
+//
+//  Created by spw on 9/13/15.
+//  Copyright (c) 2015 spw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
