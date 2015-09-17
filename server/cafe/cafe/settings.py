c37d5735488c45d0680b8898472ff00232fa9711
noahcharris <noahharris@deepsprings.edu>
2015-09-17 00:17:40 -0700

    using a different db now

--- server/cafe/cafe/settings.py
+++ server/cafe/cafe/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'cafe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'noahharris',
-        'USER': 'noahharris',
+        'NAME': 'cafedb',
+        'USER': '',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
--- server/cafe/main/views.py
+++ server/cafe/main/views.py
@@ -15,7 +15,9 @@ import math
 
 
 def createUser(request):
-  print(request.GET)
+
+
+
   #this is how we get queryArgs
   fn = request.GET['fn']
   ln = request.GET['ln']
@@ -24,7 +26,12 @@ def createUser(request):
   email = request.GET['e']
   password = request.GET['p']
 
-  u = User(createdAt=timezone.now(), inPool=True, signedIn=True, firstName=fn, lastName=ln, age=age, sex=sex, email=email, password=password, totalMatches=0, missedMatches=0)
+
+
+  # TODO - check to make sure email/username isn't taken before creating
+
+
+  u = User(createdAt=timezone.now(), inPool=False, signedIn=True, firstName=fn, lastName=ln, age=age, sex=sex, email=email, password=password, totalMatches=0, missedMatches=0)
 
   u.save()
 
@@ -56,52 +63,50 @@ def signUp(request):
 
 def findMatch(request):
 
-  #check to make sure they reviewed their last match
+  # TODO - check to make sure they reviewed their last match
 
-  #run the search algorithm for the specified user
+  #TODO - run the search algorithm for the specified user
 
   #find the midpoint between the two users' latlons
-  m = midpoint(x1, y1, x2, y2)
+  #m = midpoint(x1, y1, x2, y2)
 
-  #use this midpoint and some specified radius for the Yelp query
+  #TODO - use this midpoint and some specified radius for the Yelp query
 
   #r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
   r =  requests.get('https://google.com')
   print('wooo'+ str(r.status_code))
   print(r.cookies)
 
+  #TODO - create match in the db, (create location if need to?)
+
   response = JsonResponse({'matches': 'none'})
   return response
 
 
 
-
-
 def reviewMatch(request):
-  #add the rating to the proper match
+  #TODO - add the rating to the proper match
   response = JsonResponse({'thanks': 'for reviewing'})
   return response
 
 
 
-
-def midpoint(x1, y1, x2, y2):
 #Input values as degrees
-
+def midpoint(x1, y1, x2, y2):
 #Convert to radians
-    lat1 = math.radians(x1)
-    lon1 = math.radians(x2)
-    lat2 = math.radians(y1)
-    lon2 = math.radians(y2)
+  lat1 = math.radians(x1)
+  lon1 = math.radians(x2)
+  lat2 = math.radians(y1)
+  lon2 = math.radians(y2)
 
-    bx = math.cos(lat2) * math.cos(lon2 - lon1)
-    by = math.cos(lat2) * math.sin(lon2 - lon1)
-    lat3 = math.atan2(math.sin(lat1) + math.sin(lat2), \
-           math.sqrt((math.cos(lat1) + bx) * (math.cos(lat1) \
-           + bx) + by**2))
-    lon3 = lon1 + math.atan2(by, math.cos(lat1) + Bx)
+  bx = math.cos(lat2) * math.cos(lon2 - lon1)
+  by = math.cos(lat2) * math.sin(lon2 - lon1)
+  lat3 = math.atan2(math.sin(lat1) + math.sin(lat2), \
+         math.sqrt((math.cos(lat1) + bx) * (math.cos(lat1) \
+         + bx) + by**2))
+  lon3 = lon1 + math.atan2(by, math.cos(lat1) + Bx)
 
-    return [round(math.degrees(lat3), 2), round(math.degrees(lon3), 2)]
+  return [round(math.degrees(lat3), 2), round(math.degrees(lon3), 2)]
 
 
 
